0ce081b48c1bc7a92bd6ca72864d6db7b9a9cde1
kurlyk818 <baranovskiys2019@gmail.com>
2025-05-26 00:31:50 +0300

    Please p

---
+++ config.js
@@ -0,0 +1,233 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+	// ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+	// ├┴┐├─┤└─┐││  └─┐
+	// └─┘┴ ┴└─┘┴└─┘└─┘
+
+	// General
+	name: 'nekojidev',
+	imageBackground: false,
+	openInNewTab: true,
+	twelveHourFormat: true,
+	timeZone: 'utc+2',
+
+	// Greetings
+	greetingMorning: 'Good morning!',
+	greetingAfternoon: 'Good afternoon,',
+	greetingEvening: 'Good evening,',
+	greetingNight: 'Go to Sleep!',
+
+	// Layout
+	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+
+	// Weather
+	weatherKey: '8b05d62206f459e1d298cbe5844d7d87', // Write here your API Key
+	weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherUnit: 'C', // 'F', 'C'
+	language: 'en', // More languages in https://openweathermap.org/current#multi
+
+	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
+	defaultLatitude: '19.593',
+	defaultLongitude: '-99.250',
+
+	// Autochange
+	autoChangeTheme: false,
+
+	// Autochange by OS
+	changeThemeByOS: true,
+
+	// Autochange by hour options (24hrs format, string must be in: hh:mm)
+	changeThemeByHour: false,
+	hourDarkThemeActive: '18:30',
+	hourDarkThemeInactive: '07:00',
+
+	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+	// ├┴┐│ │ │  │ │ ││││└─┐
+	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+	firstButtonsContainer: [
+		{
+			id: '1',
+			name: 'Github',
+			icon: 'github',
+			link: 'https://github.com/',
+		},
+		{
+			id: '2',
+			name: 'Mail',
+			icon: 'mail',
+			link: 'https://mail.google.com/mail/u/0/#inbox',
+		},
+		{
+			id: '3',
+			name: 'classroom',
+			icon: 'trello',
+			link: 'https://classroom.google.com/u/1/',
+		},
+		{
+			id: '4',
+			name: 'Calendar',
+			icon: 'popcorn',
+			link: 'https://uakino.me/',
+		},
+		{
+			id: '5',
+			name: 'work',
+			icon: 'briefcase',
+			link: 'http://localhost:3000/',
+		},
+		{
+			id: '6',
+			name: 'YouTube',
+			icon: 'play',
+			link: 'https://ua.udemy.com/organization/home/',
+		},
+	],
+
+	secondButtonsContainer: [
+		{
+			id: '1',
+			name: 'Music',
+			icon: 'headphones',
+			link: 'https://open.spotify.com',
+		},
+		{
+			id: '2',
+			name: 'twitter',
+			icon: 'twitter',
+			link: 'https://twitter.com/',
+		},
+		{
+			id: '3',
+			name: 'bot',
+			icon: 'bot',
+			link: 'https://discord.com/app',
+		},
+		{
+			id: '4',
+			name: 'Amazon',
+			icon: 'shopping-bag',
+			link: 'https://amazon.com/',
+		},
+		{
+			id: '5',
+			name: 'Hashnode',
+			icon: 'pen-tool',
+			link: 'https://hashnode.com/',
+		},
+		{
+			id: '6',
+			name: 'Figma',
+			icon: 'figma',
+			link: 'https://figma.com/',
+		},
+	],
+
+	// ┬  ┬┌─┐┌┬┐┌─┐
+	// │  │└─┐ │ └─┐
+	// ┴─┘┴└─┘ ┴ └─┘
+
+	// First Links Container
+	firstlistsContainer: [
+		{
+			icon: 'star',
+			id: '1',
+			links: [
+				{
+					name: 'jut-su',
+					link: 'https://jut.su/',
+				},
+				{
+					name: 'AnimeStars',
+					link: 'https://animestars.org/',
+				},
+				{
+					name: 'Claude',
+					link: 'https://claude.ai/new',
+				},
+				{
+					name: 'codewars',
+					link: 'https://www.codewars.com/dashboard',
+				},
+
+			],
+		},
+		{
+			icon: 'tv',
+			id: '2',
+			links: [
+				{
+					name: 'YouTube',
+					link: 'https://www.youtube.com',
+				},
+				{
+					name: 'ChatGPT',
+					link: 'https://chat.openai.com/',
+				},
+				{
+					name: 'Claude',
+					link: 'https://claude.ai/new',
+				},
+				{
+					name: 'Twitch',
+					link: 'https://www.twitch.tv',
+				},
+
+			],
+		},
+	],
+
+	// Second Links Container
+	secondListsContainer: [
+		{
+			icon: 'square-user-round',
+			id: '1',
+			links: [
+				{
+					name: 'Spotify',
+					link: 'https://www.spotify.com',
+				},
+				{
+					name: 'r/unixporn',
+					link: 'https://www.reddit.com/r/unixporn',
+				},
+				{
+					name: 'Facebook',
+					link: 'https://www.facebook.com',
+				},
+				{
+					name: 'Twitter',
+					link: 'https://twitter.com',
+				},
+			],
+		},
+		{
+			icon: 'binary',
+			id: '2',
+			links: [
+				{
+					name: 'Programming',
+					link: 'https://www.reddit.com/r/programming/',
+				},
+				{
+					name: 'Rust',
+					link: 'https://www.reddit.com/r/rust/',
+				},
+				{
+					name: 'Go',
+					link: 'https://www.reddit.com/r/golang/',
+				},
+				{
+					name: 'Python',
+					link: 'https://www.reddit.com/r/Python/',
+				},
+			],
+		},
+	],
+};
